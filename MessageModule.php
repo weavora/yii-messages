27026590633250e0159ecbdf780907c115ed44f5
Alex Hudenko <mistulya@gmail.com>
2011-11-22 16:06:17 +0200

    add checks for user defined methods

--- MessageModule.php
+++ MessageModule.php
@@ -5,10 +5,35 @@ class MessageModule extends CWebModule
 
 	public $userModel = 'User';
 	public $userModelRelation = null;
-//	public $defaultController = 'Message';
+	public $getNameMethod;
+	public $getSuggestMethod;
+
+	public $senderRelation;
+	public $receiverRelation;
 
 	public function init()
 	{
+
+		if (!$this->getNameMethod) {
+			throw new Exception(MessageModule::t("Property MessageModule::getNameMethod not defined"));
+		}
+
+		if (!$this->getSuggestMethod) {
+		    throw new Exception(MessageModule::t("Property MessageModule::getSuggest not defined"));
+		}
+
+		if (!class_exists($this->userModel)) {
+		    throw new Exception(MessageModule::t("Class {userModel} not defined", array('{userModel}' => $this->userModel)));
+		}
+
+		if (!method_exists($this->userModel, $this->getNameMethod)) {
+		    throw new Exception(MessageModule::t("Method {userModel}::{getNameMethod} not defined", array('{userModel}' => $this->userModel, '{getNameMethod}' => $this->getNameMethod)));
+		}
+
+		if (!is_callable(array($this->userModel, $this->getNameMethod), true)) {
+		    throw new Exception(MessageModule::t("Method {userModel}::{getNameMethod} can not defined", array('{userModel}' => $this->userModel, '{getNameMethod}' => $this->getNameMethod)));
+		}
+
 		// this method is called when the module is being created
 		// you may place code here to customize the module or the application
 
@@ -36,7 +61,7 @@ class MessageModule extends CWebModule
 		}
 	}
 
-	public static function t($str='',$params=array(),$dic='user') {
+	public static function t($str='',$params=array(),$dic='message') {
 		return Yii::t("MessageModule.".$dic, $str, $params);
 	}
 
