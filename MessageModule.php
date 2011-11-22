5dc505af824ee38cdce085db5707dbcd18bed88c
Ivan Dragun <alex-s@mailinator.com>
2011-11-22 14:18:24 +0300

    redirect guest users to defined login page

--- MessageModule.php
+++ MessageModule.php
@@ -21,14 +21,19 @@ class MessageModule extends CWebModule
 
 	public function beforeControllerAction($controller, $action)
 	{
-		if(parent::beforeControllerAction($controller, $action) && !Yii::app()->user->isGuest)
-		{
+		if (Yii::app()->user->isGuest) {
+			if (Yii::app()->user->loginUrl) {
+				$controller->redirect($controller->createUrl(reset(Yii::app()->user->loginUrl)));
+			} else {
+				$controller->redirect($controller->createUrl('/'));
+			}
+		} else if (parent::beforeControllerAction($controller, $action)) {
 			// this method is called before any module controller action is performed
 			// you may place customized code here
 			return true;
-		}
-		else
+		} else {
 			return false;
+		}
 	}
 
 	public static function t($str='',$params=array(),$dic='user') {
