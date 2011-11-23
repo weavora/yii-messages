d9dc900cd72363b0fa20f6c644d66e0a4876d653
Alex Hudenko <mistulya@gmail.com>
2011-11-23 17:22:08 +0200

    fix userModels iteration

--- controllers/SuggestController.php
+++ controllers/SuggestController.php
@@ -6,13 +6,14 @@ class SuggestController extends Controller {
 		$q = Yii::app()->request->getParam('name_startsWith');
 		$userModels = call_user_func(array(User::model(), Yii::app()->getModule('message')->getSuggestMethod), $q);
 		$users = array();
-		foreach ($userModels as $userModel) {
+		if ($userModels) {
+		    foreach ($userModels as $userModel) {
 			$users[] = array(
 				'id' => $userModel->getPrimaryKey(),
 				'name' => call_user_func(array($userModel, $this->getModule()->getNameMethod))
 			);
+			}
 		}
-
 		$json = CJSON::encode(array('users' => $users));
 
 		if (Yii::app()->request->getParam('callback')) {
